590fd7ab7a83ad600da5aa469013942fb773d882
samsher2578 <samsherkhan2578@gmail.com>
2024-02-04 11:10:05 +0530

    hero-left update

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -2,7 +2,7 @@ import chevronLeft from "./chevron-left.svg";
 import cheveronRight from "./chevron-right.svg";
 import copyrightSign from "./copyright-sign.svg";
 import facebook from "./facebook.svg";
-import hamburger from "./hamburger.svg";
+
 import menu from "./menu.png";
 import instagram from "./instagram.svg";
 import support from "./support.svg";
@@ -20,7 +20,6 @@ export {
   chevronLeft,
   copyrightSign,
   facebook,
-  hamburger,
   instagram,
   support,
   shieldTick,
